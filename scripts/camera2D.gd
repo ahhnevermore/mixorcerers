75511784a219dcc372c6d4cd3e649c505dd0d4e1
kryzel <correspondence.nu@gmail.com>
2024-03-23 06:22:42 +0530

    camera done

--- scripts/camera2D.gd
+++ scripts/camera2D.gd
@@ -1,12 +1,16 @@
 extends Camera2D
 
 var cursor:Cursor
+var map:Map
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	cursor = get_parent().get_node('Cursor')
+	map = get_parent().get_node('Map')
 	position=cursor.position
-	self.limit_left = -150
-	limit_top = -150
+	limit_left = -50
+	limit_top = -50
+	limit_bottom = map.map_to_local(Vector2i(map.xw,map.yw)).y +50
+	limit_right = map.map_to_local(Vector2i(map.xw,map.yw)).x +50
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
@@ -14,5 +18,8 @@ func _process(_delta):
 
 func _on_cursor_changed():
 	var centre = get_screen_center_position()
-	if abs(centre.x - cursor.cursor_tile.x)  > int((abs(centre.x - limit_left) *2)/3) :
+	if (
+		(abs(centre.x - cursor.cursor_tile.x)  > int((abs(centre.x - limit_left) *2)/3)) or
+		(abs(centre.x - cursor.cursor_tile.x)  > int((abs(centre.x - limit_right) *2)/3)) 
+	):
 		position = cursor.position
